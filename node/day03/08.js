287d76dec13c792e21dea8eed380349c2b14a623
liqian <liqian@baihe.com>
2019-03-25 21:07:10 +0800

    hanxs

---
+++ node/day03/08.js
@@ -0,0 +1,10 @@
+
+
+
+var sd = require('silly-datetime');
+
+var ttt =  sd.format(new Date(), 'YYYY-MM-DD HH:mm');
+
+console.log(ttt);
+var sd = require('silly-datetime');
+sd.format(new Date(), 'YYYY-MM-DD HH:mm');
