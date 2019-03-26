ea8e56ac723aab4101379d241c88c009dc8ed5dc
xiaosong <xiaosong@xiaosongdeMacBook-Pro.local>
2019-03-26 08:13:10 +0800

    hanxs

--- node/day03/router.js
+++ node/day03/router.js
@@ -1,5 +1,31 @@
 
-exports.showIndex = function (req,res) {
+// exports.showIndex = function (req,res) {
+//
+//     res.writeHeader(200,{'Content-Type':'text/html;charset=UTF-8'});
+//
+//     res.end('我是首页');
+//
+// }
+//
+// exports.showStudent = function (req,res) {
+//
+//     var id = req.url.substr(9,6);
+//
+//     res.writeHeader(200,{'Content-Type':'text/html;charset=UTF-8'});
+//
+//     res.end('我是学生页面'+ id);
+//
+// }
+//
+// exports.show404 = function (req,res) {
+//
+//     res.writeHeader(404,{'Content-Type':'text/html;charset=UTF-8'});
+//
+//     res.end('404,页面没有找到');
+// }
+
+
+var showIndex = function (req,res) {
 
     res.writeHeader(200,{'Content-Type':'text/html;charset=UTF-8'});
 
@@ -7,7 +33,7 @@ exports.showIndex = function (req,res) {
 
 }
 
-exports.showStudent = function (req,res) {
+var showStudent = function (req,res) {
 
     var id = req.url.substr(9,6);
 
@@ -17,9 +43,19 @@ exports.showStudent = function (req,res) {
 
 }
 
-exports.show404 = function (req,res) {
+var show404 = function (req,res) {
 
     res.writeHeader(404,{'Content-Type':'text/html;charset=UTF-8'});
 
     res.end('404,页面没有找到');
 }
+
+
+
+
+
+exports.show404 = show404;
+
+exports.showStudent = showStudent;
+
+exports.showIndex = showIndex;
